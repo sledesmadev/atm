4027eb7ec4daa952f64f76d1a8330f2eec745a1f
sledesmadev <sledesmadev@gmail.com>
2022-02-06 19:43:17 +0100

    First commit

---
+++ src/main/java/com/neueda/atm/NeuedaAtmMachineApplication.java
@@ -0,0 +1,13 @@
+package com.neueda.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuedaAtmMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuedaAtmMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neueda/atm/NeuedaAtmMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuedaAtmMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
